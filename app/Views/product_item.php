ad5ec0fc1760cc93aec1b0f08a773ea9bed0195e
fawwazabrials <18221067@std.stei.itb.ac.id>
2023-12-15 13:29:20 +0700

    style: change form size

--- app/Views/product_item.php
+++ app/Views/product_item.php
@@ -1,8 +1,8 @@
 <?= $this->extend('layout') ?>
 
 <?= $this->section('content') ?>
-    <div class="flex flex-row gap-8">
-        <div class="shrink-0">
+    <div class="flex flex-col sm:flex-row    gap-8">
+        <div>
             <img src="/img/vegetable.jpg" alt="Gambar sayuran" width="300" height="300" class="object-cover rounded-lg shadow-xl">
         </div>
 
@@ -10,7 +10,7 @@
             <h1 class="text-3xl font-bold"><?= esc($product['name']) ?></h1>
             <p class="text-md text-slate-400"><?= esc($product['variant']) ?></p>
     
-            <div>
+            <div class="max-w-[400px]">
                 <div>
                     <h2 class='font-semibold mt-4'>Deskripsi Produk</h2>
                     <p><?= str_replace('\n', '<br/>', esc($product['description'])) ?></p>
@@ -26,19 +26,16 @@
             </div>
         </div>
 
-        <div class="card border border-slate-300 w-[1000px]">
+        <div class="card border border-slate-300 grow">
             <div class="card-body">
-
                 <?= form_open('product/'.esc($product['id'])) ?>
-                    <!-- <input id="productId" name="productId" class='invisible' type="text" value="<?= esc($product['id']) ?>"> -->
-
                     <h2 class="card-title font-semibold text-base">Atur data pengiriman</h2>
-                    <label class="form-control w-full max-w-xs">
+                    <label class="form-control">
                         <div class="flex flex-col mt-2">
                             <span class="label-text font-semibold">Namamu</span>
                             <span class="label-text text-red-500 text-sm"> <?= validation_show_error('customerName') ?> </span>
                         </div>
-                        <input id="customerName" name="customerName" type="text" placeholder="Isi nama" class="input input-bordered w-full max-w-xs" />
+                        <input id="customerName" name="customerName" type="text" placeholder="Isi nama" class="input input-bordered w-full" />
 
                         <div class="flex flex-col mt-2">
                             <span class="label-text font-semibold">Alamatmu</span>
@@ -50,12 +47,12 @@
                     <div class="divider"></div> 
 
                     <h2 class="card-title font-semibold text-base">Atur jumlah pengisian</h2>
-                    <label>
+                    <label class="form-control">
                         <div class="flex flex-col mt-2">
                             <span class="label-text font-semibold">Kuantitas</span>
                             <span class="label-text text-red-500 text-sm"> <?= validation_show_error('totalAmount') ?> </span>
                         </div>
-                        <input id="totalAmount" name="totalAmount" type="number" placeholder="0" class="input input-bordered w-full max-w-xs" />
+                        <input id="totalAmount" name="totalAmount" type="number" placeholder="0" class="input input-bordered w-full" />
                         <div>
                             <span class="label-text-alt"></span>
                             <span class="label-text-alt">Stok: <?= esc($product['stock']) ?></span>
